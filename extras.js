6c3c19a2e23887c0bf2f63f7e465251e69d9230b
Tristan <tristan.dijkstra@gmail.com>
2022-01-24 12:24:35 +0100

    edit

---
+++ extras.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#openAbout").animate({ background: "none", color: "white" }, "slow", function () {
+        $(this).removeClass("itemHighlight");
+    })
+
+    $("#closeAbout").click(function () {
+        $(".about").animate({ top: "-102vh"}, "slow", function () {
+            $(this).hide();
+        });
+    });
+    $("#openAbout").click(function () {
+        $(".about").show(function () {
+            $(this).animate({ top: "0" });
+        });
+    });
+    
+    
+    $("#map").load("kepler.html");
+});
